8e4ef55faaee065d8b4851435a22feb9de0e44e6
aditya1251070196-ctrl <aditya.1251070196@vit.edu>
2026-01-18 21:42:47 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "traffic-sign-app-v21";// 🔁 bump on every deploy
+const CACHE_NAME = "traffic-sign-app-v22";// 🔁 bump on every deploy
 
 const ASSETS = [
   "./",
@@ -111,6 +111,7 @@ self.addEventListener("fetch", (e) => {
     })
   );
 });
+
 
 
 
